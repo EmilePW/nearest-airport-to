44b4b1710ab6fb9bd55c558d464bec3446f23c6d
EmilePW <emilepw@hotmail.co.uk>
2017-12-19 14:40:31 +0000

    Find and display nearest airports for searched location

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const router = express.Router();
 const app = express();
 const { Q } = require("./db/");
+const geocode = require("./lib/geocode");
 
 app.use(bodyParser.json());
 app.use(
@@ -19,10 +20,16 @@ router.get("/", function(req, res) {
   res.render("index", { GOOGLE_MAPS_API_KEY: process.env.GOOGLE_MAPS_API_KEY });
 });
 
-router.get("/nearestAirport", function(req, res) {
+router.get("/nearestAirport", async function(req, res) {
   const { location } = req.query;
-
-})
+  const { lat, lng } = await geocode.getLatLng(location);
+  // Find airports within 150 miles
+  var nearestAirports = await Q.airports.findWithinRadius(lat, lng, 150);
+  res.render("index", {
+    GOOGLE_MAPS_API_KEY: process.env.GOOGLE_MAPS_API_KEY,
+    nearestAirports: nearestAirports
+  });
+});
 
 app.listen(process.env.PORT, function() {
   console.log(`Listening on port ${process.env.PORT}`);
