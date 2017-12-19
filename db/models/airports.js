9d962b569e8e481e014ed228a43225a049882816
EmilePW <emilepw@hotmail.co.uk>
2017-12-19 14:49:05 +0000

    Order airports by distance (ascending)

--- db/models/airports.js
+++ db/models/airports.js
@@ -5,7 +5,8 @@ module.exports = {
     sql: function(lat, lng, radius) {
       return `
         SELECT name, iata_code, point(${lng}, ${lat}) <@> lng_lat AS distance FROM airports
-        WHERE point(${lng}, ${lat}) <@> lng_lat < ${radius}`;
+        WHERE point(${lng}, ${lat}) <@> lng_lat < ${radius}
+        ORDER BY distance ASC`;
     },
     postQuery: function(result) {
       return result.rows;
